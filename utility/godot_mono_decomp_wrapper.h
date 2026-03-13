e42c6ef9376758b822cadfea303245cfaabde97d
nikitalita <69168929+nikitalita@users.noreply.github.com>
2026-03-13 11:37:13 -0700

    add GodotMonoDecompWrapper::get_files_in_file_map()

--- utility/godot_mono_decomp_wrapper.h
+++ utility/godot_mono_decomp_wrapper.h
@@ -46,6 +46,7 @@ public:
 	String decompile_individual_file(const String &file);
 	Dictionary get_script_info(const String &file);
 	Vector<String> get_files_not_present_in_file_map();
+	Vector<String> get_files_in_file_map();
 	Vector<String> get_all_strings_in_module();
 
 	GodotMonoDecompSettings get_settings() const;
