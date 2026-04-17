4f5b6f38949734eb7e5cce0d8dee41938145b82f
nikitalita <69168929+nikitalita@users.noreply.github.com>
2026-04-17 06:37:18 -0700

    vtracer: Only re-create header if it doesn't match

--- external/vtracer/build.rs
+++ external/vtracer/build.rs
@@ -9,18 +9,6 @@ fn main() {
 
     let package_name = env::var("CARGO_PKG_NAME").expect("CARGO_PKG_NAME env var is not defined");
 
-    // let build_dir = PathBuf::from(
-    //     env::var("CARGO_TARGET_DIR").expect("CARGO_TARGET_DIR env var is not defined"),
-    // );
-    // // println!("cargo:warning=build_dir: {}", build_dir.display());
-
-
-    // // print out the entire environment
-    // for (key, value) in env::vars() {
-    //     println!("cargo:warning={}={}", key, value);
-    // }
-
-
     let config = cbindgen::Config::from_file("cbindgen.toml")
         .expect("Unable to find cbindgen.toml configuration file");
 
@@ -31,14 +19,31 @@ fn main() {
 
         let header_file = format!("{}.h", package_name);
         let header_path = if let Ok(target_dir) = env::var("CBINDGEN_TARGET_DIR") {
-            std::fs::create_dir_all(target_dir.clone()).expect("Failed to create target directory");
+			if !std::path::Path::new(&target_dir).exists() {
+				std::fs::create_dir_all(target_dir.clone()).expect("Failed to create target directory");
+			}
             PathBuf::from(target_dir).join(header_file.clone())
         } else {
-            PathBuf::from("include").join("vtracer").join(header_file.clone())
+            PathBuf::from("include")
+                .join("vtracer")
+                .join(header_file.clone())
         };
-        println!("cargo:warning=header_path: {}", header_path.display());
+        // Write to an in-memory buffer first, then compare it to the expected content.
+        let mut generated_bytes = Vec::new();
+        writer.write(&mut generated_bytes);
+        let generated_content =
+            String::from_utf8(generated_bytes).expect("Failed to decode generated header content");
 
-        // ensure target_dir exists
-        writer.write_to_file(header_path);
+        if header_path.exists() {
+            let expected_content = std::fs::read_to_string(header_path.clone())
+                .expect("Failed to read expected content");
+            if expected_content == generated_content {
+                println!("Header content matches expected content, not regenerating");
+                return;
+            }
+        }
+
+        println!("cargo:warning=Regenerated header at path: {}", header_path.display());
+        std::fs::write(header_path, generated_content).expect("Failed to write generated header to file");
     }
 }
