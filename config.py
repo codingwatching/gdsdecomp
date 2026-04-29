34f54349590bb95feb8c0a6444f3a7c9fa551cf3
nikitalita <69168929+nikitalita@users.noreply.github.com>
2026-04-29 14:34:23 -0700

    update documentation

--- config.py
+++ config.py
@@ -103,6 +103,7 @@ def get_doc_classes():
     return [
         "GDScriptDecomp",
         "Glob",
+        "SemVer",
         "GodotVer",
         "GodotREEditorStandalone",
         "ImportExporter",
